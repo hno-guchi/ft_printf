5e3725f1f325f3ee0cb5f4c6c07fbb2481a62c3c
Hidenobu <>
2022-06-10 21:42:12 +0900

    Add buffering

--- srcs/ftp_treat_string.c
+++ srcs/ftp_treat_string.c
@@ -6,20 +6,22 @@
 /*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 12:24:56 by hnoguchi          #+#    #+#             */
-/*   Updated: 2022/06/08 17:37:32 by hnoguchi         ###   ########.fr       */
+/*   Updated: 2022/06/10 17:26:14 by hnoguchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
-int	ftp_treat_string(char *str)
+int	ftp_treat_string(char *str, size_t len)
 {
-	int	n;
+	int		n;
 
 	if (str == NULL)
 		str = "(null)";
 	n = ft_strlen(str);
-	ft_putstr_fd(str, 1);
+	if (INT_MAX <= (len + n))
+		return (-1);
+	(void)write(1, str, n);
 	return (n);
 }
