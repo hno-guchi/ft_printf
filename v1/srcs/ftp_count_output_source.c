5a2f9204c67435dc1fd3523d9c2b34c78c928f38
Hidenobu <>
2022-06-15 10:20:37 +0900

    Process [% conv][string]

---
+++ v1/srcs/ftp_count_output_source.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ftp_count_output.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnoguchi <hnoguchi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 10:18:52 by hnoguchi          #+#    #+#             */
+/*   Updated: 2022/06/10 10:26:26 by hnoguchi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include "libft.h"
+
+int	ftp_count_output(const char *save, va_list args)
+{
+	size_t		i;
+	size_t		c;
+
+	i = 0;
+	c = 0;
+	while (save[i] != '\0')
+	{
+		if (save[i] == '%')
+		{
+			i += 1;
+			c += ftp_treat_something(save[i], args);
+		}
+		else
+		{
+			ft_putchar_fd(save[i], 1);
+			c += 1;
+		}
+		i += 1;
+		if (save[i] == '\0')
+			return (len);
+	}
+	return (c);
+}
